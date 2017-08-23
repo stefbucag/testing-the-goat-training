98db32f791325cc19799c762dbabc5029d3bb1af
stefbucag <msibucag@gmail.com>
2017-08-23 17:35:08 +0800

    A separate template to display lists

---
+++ lists/tests/test_list_view.py
@@ -0,0 +1,23 @@
+from django.http import HttpRequest
+from django.urls import resolve
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+from lists.views import home_page
+from lists.models import Item
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
